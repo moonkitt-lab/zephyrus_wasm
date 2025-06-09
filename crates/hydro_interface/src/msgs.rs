5f2ddf37fd619d65619b0b45a28456d7f1c6a5a9
Victor118 <3109292+Victor118@users.noreply.github.com>
2025-06-09 15:05:12 +0200

    Receive nft in progress

--- crates/hydro_interface/src/msgs.rs
+++ crates/hydro_interface/src/msgs.rs
@@ -1,4 +1,5 @@
 use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, Coin, Timestamp, Uint128};
 
 #[cw_serde]
 pub enum ExecuteMsg {
@@ -15,4 +16,26 @@ pub enum ExecuteMsg {
 }
 
 #[cw_serde]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    SpecificUserLockups { address: String, lock_ids: Vec<u64> },
+}
+
+#[cw_serde]
+pub struct SpecificUserLockupsResponse {
+    pub lockups: Vec<LockEntryWithPower>,
+}
+
+#[cw_serde]
+pub struct LockEntryWithPower {
+    pub lock_entry: LockEntryV2,
+    pub current_voting_power: Uint128,
+}
+
+#[cw_serde]
+pub struct LockEntryV2 {
+    pub lock_id: u64,
+    pub owner: Addr,
+    pub funds: Coin,
+    pub lock_start: Timestamp,
+    pub lock_end: Timestamp,
+}
